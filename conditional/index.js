300ae178e545763ecf6712f07ae8ae92b1c6cdc3
brahmbhatt maithili07 <8505barotmaithili@gmail.com>
2024-10-22 16:27:21 +0530

    Add files via upload

---
+++ conditional/index.js
@@ -0,0 +1,106 @@
+
+// conditional operator
+
+// if condition
+
+// var a=30;
+// var b=40;
+
+// if(a<b)
+// {
+//     console.log("a is greater");
+// }
+
+// if..else condition
+
+
+
+// var a=30;
+// var b=40;
+
+// if(a>b)
+// {
+//     console.log("a is greater");
+// }
+// else{
+//     console.log("a  is not greater");
+// }
+
+
+
+// if..else if condition
+
+// var rice_available = false;
+// var wheat_available = true;
+// var apple_available = true;
+// if (rice_available) {
+//   console.log("Buy rice");
+// } else if (wheat_available) {
+//   console.log("Buy Wheat");
+// } else if (apple_available) {
+//   console.log("Buy apple");
+// } else {
+//   console.log("Nothing is available");
+// }
+
+
+// nestead if
+
+// var a=1
+// var b=2
+//     if (a === 1) {
+//       if (b === 2) {
+//         console.log("a is 1 and b is 2");
+//       }
+//     } else {
+//       console.log("a is not 1");
+//     }
+
+// var a=60;
+// var b=70;
+
+// if(a<b){
+//     if(b<a){
+//         console.log("good morning");
+//     }
+//     else{
+//         console.log("good evening");
+//     }
+// }
+
+
+  
+
+
+//ternary operator
+
+// let a=20;
+// let b=25;
+
+
+// (a>18 && b>18)?console.log("eligible"):console.log("not eligible")
+
+
+
+// let a=50;
+// let b=20;
+
+// (a>b && b<a)?console.log("true"):console.log("false")
+
+
+
+// let a=50;
+// let b=40;
+// let c=30;
+// let d=20;
+
+// (a>b && a>c && a>d)?console.log("true"):console.log("false")
+
+
+// let a=1;
+// let b=2;
+// let c=3;
+// let d=4;
+// let e=5;
+
+// (a>0 || a<b || a>c || a >e)?console.log("pass"):console.log("fail")
